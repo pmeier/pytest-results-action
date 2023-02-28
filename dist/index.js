6adbb99334fac63dccfeeb4ef088ac13c12ae651
Philip Meier <github.pmeier@posteo.de>
2023-02-28 17:53:31 +0100

    fix formatting

--- dist/index.js
+++ dist/index.js
@@ -4863,37 +4863,20 @@ const { readFileSync } = __nccwpck_require__(7561);
 
 const junit_xml = core.getInput("junit-xml", { required: true });
 
-function addDetailsWithCodeBlock(summary, label, code, lang) {
+function addDetailsWithCodeBlock(summary, label, code) {
   return summary.addDetails(
     label,
-    summary.wrap(
-      "pre",
-      summary.wrap("code", code.trim()),
-      Object.assign({}, lang && { lang })
-    )
+    "\n\n" + summary.wrap("pre", summary.wrap("code", code))
   );
-}
-
-const htmlEntities = {
-  nbsp: " ",
-  lt: "<",
-  gt: ">",
-  amp: "&",
-  quot: '"',
-  apos: "'",
-};
-const htmlEntityPattern = new RegExp("&([a-z]{2,4});", "g");
-
-function decodeHtmlEntities(text) {
-  return text.replace(htmlEntityPattern, function (match, entity) {
-    return Object.hasOwn(htmlEntities, entity) ? htmlEntities[entity] : match;
-  });
 }
 
 const main = async () => {
   core.summary.addHeading("Tests");
 
-  const parser = new XMLParser({ ignoreAttributes: false });
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    processEntities: false,
+  });
   const results = parser.parse(readFileSync(junit_xml, "utf-8"));
 
   for (const result of results.testsuites.testsuite.testcase) {
@@ -4904,7 +4887,7 @@ const main = async () => {
           "code",
           result["@_classname"] + "." + result["@_name"]
         ),
-        core.summary.wrap("code", decodeHtmlEntities(result.failure["#text"]))
+        result.failure["#text"]
       );
     }
   }
