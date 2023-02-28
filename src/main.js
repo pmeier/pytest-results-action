e6d9ef30b0c1a45c43eba4c8926bbc2de059f642
Philip Meier <github.pmeier@posteo.de>
2023-02-28 09:06:02 +0100

    make this a javascript action

---
+++ src/main.js
@@ -0,0 +1,36 @@
+const core = require('@actions/core');
+const {XMLParser} = require('fast-xml-parser');
+const {readFileSync} = require('node:fs');
+
+//const junit_xml = core.getInput('junit-xml', { required: true });
+const junit_xml = "test-results.xml";
+process.env.GITHUB_STEP_SUMMARY = "summary.log";
+
+function addDetailsWithCodeBlock(summary, label, code, lang) {
+    return summary.addDetails(
+        label,
+        summary.wrap('pre', summary.wrap('code', code.trim()), Object.assign({}, (lang && { lang }))),
+    )
+}
+
+const main = async () => {
+  core.summary.addHeading('Tests')
+
+  const parser = new XMLParser({ignoreAttributes : false});
+  const results = parser.parse(readFileSync(junit_xml, "utf-8")).testsuites.testsuite.testcase
+
+  for (const result of results) {
+    if (Object.hasOwn(result, "failure")) {
+        addDetailsWithCodeBlock(core.summary, core.summary.wrap("code", result["@_classname"] + "." + result["@_name"]),
+        core.summary.wrap("code", result.failure["#text"]),
+        )
+
+        console.log(result.failure["#text"])
+    }
+}
+
+  await core.summary.write()
+};
+
+main();
+
