d23992c63aa9a67a7467af0c3302500d2e8c3475
Philip Meier <github.pmeier@posteo.de>
2023-03-03 16:55:48 +0100

    cleanup

--- dist/index.js
+++ dist/index.js
@@ -6,8 +6,6 @@
 
 "use strict";
 
-assignAttributes
-
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
