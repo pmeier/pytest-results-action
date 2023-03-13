cc0a5ceed04a3ccbb0445f2e6d4186338a6bcc6b
Philip Meier <github.pmeier@posteo.de>
2023-03-13 11:22:45 +0100

    add support for specifying multiple paths including dirs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,80 @@
-const core = require("@actions/core");
-const { XMLParser } = require("fast-xml-parser");
-const { readFileSync } = require("node:fs");
+const fs = require("fs").promises;
 
-const junit_xml = core.getInput("junit-xml", { required: true });
+const core = require("@actions/core");
+const glob = require("@actions/glob");
+
+const { XMLParser } = require("fast-xml-parser");
+
+const main = async () => {
+  const path = core.getInput("path", { required: true });
+  const failOnEmpty = core.getBooleanInput("fail-on-empty", {
+    required: false,
+  });
+
+  const files = await getFiles(path);
+  if (files.length == 0 && failOnEmpty) {
+    core.setFailed(
+      "No JUnit XML file was found. Set `fail-on-empty: false` if that is a valid use case"
+    );
+  }
+
+  await postResults(files);
+};
+
+main();
+
+async function getFiles(path) {
+  const globber = await glob.create(path, {
+    implicitDescendants: false,
+  });
+  const paths = await globber.glob();
+
+  var files = [];
+  for (const file_or_dir of paths) {
+    var stats;
+    try {
+      stats = await fs.stat(file_or_dir);
+    } catch (error) {
+      core.setFailed(`Action failed with error ${error}`);
+    }
+    if (stats.isFile()) {
+      files.push(file_or_dir);
+    } else {
+      const globber = await glob.create(file_or_dir + "/**/*.xml", {
+        implicitDescendants: false,
+      });
+      files.push(...(await globber.glob()));
+    }
+  }
+  return files;
+}
+
+async function postResults(files) {
+  core.summary.addHeading("Tests");
+
+  const parser = new XMLParser({
+    ignoreAttributes: false,
+    processEntities: false,
+  });
+  for (const file of files) {
+    const results = parser.parse(await fs.readFile(file, "utf-8"));
+
+    for (const result of results.testsuites.testsuite.testcase) {
+      if (Object.hasOwn(result, "failure")) {
+        addDetailsWithCodeBlock(
+          core.summary,
+          core.summary.wrap(
+            "code",
+            result["@_classname"] + "." + result["@_name"]
+          ),
+          result.failure["#text"]
+        );
+      }
+    }
+  }
+
+  return core.summary.write();
+}
 
 function addDetailsWithCodeBlock(summary, label, code) {
   return summary.addDetails(
@@ -10,30 +82,3 @@ function addDetailsWithCodeBlock(summary, label, code) {
     "\n\n" + summary.wrap("pre", summary.wrap("code", code))
   );
 }
-
-const main = async () => {
-  core.summary.addHeading("Tests");
-
-  const parser = new XMLParser({
-    ignoreAttributes: false,
-    processEntities: false,
-  });
-  const results = parser.parse(readFileSync(junit_xml, "utf-8"));
-
-  for (const result of results.testsuites.testsuite.testcase) {
-    if (Object.hasOwn(result, "failure")) {
-      addDetailsWithCodeBlock(
-        core.summary,
-        core.summary.wrap(
-          "code",
-          result["@_classname"] + "." + result["@_name"]
-        ),
-        result.failure["#text"]
-      );
-    }
-  }
-
-  await core.summary.write();
-};
-
-main();
